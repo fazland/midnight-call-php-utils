ed32dcecea970c3cbd72c0fd034362df5f799dc0
Massimiliano Braglia <massimiliano.braglia@gmail.com>
2019-10-24 17:05:36 +0200

    Added RequestHandlerTrait

---
+++ src/Model/RequestHandlerTrait.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+namespace App\Model;
+
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * @property FormFactoryInterface $formFactory
+ * @method string getTypeClass
+ * @method void commit
+ */
+trait RequestHandlerTrait
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function handleRequest(Request $request): object
+    {
+        $form = $this->formFactory->createNamed(null, $this->getTypeClass(), $this);
+        $form->handleRequest($request);
+        if (! $form->isSubmitted() || ! $form->isValid()) {
+            return $form;
+        }
+
+        $this->commit();
+
+        return $this;
+    }
+}
